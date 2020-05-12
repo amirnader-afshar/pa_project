91d06f3bd145dbc14ea8f799fb0e68d38a627fe9
naderenator <naderenator@gmail.com>
2020-05-12 15:37:44 +0430

    async awit add

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-process.env.NODE_CONFIG_DIR = __dirname+'/config';
+process.env.NODE_CONFIG_DIR = __dirname + "/config";
 
- const config = require('config');
-const express = require('express');
-const _ = require('lodash');
+const config = require("config");
+const express = require("express");
+const _ = require("lodash");
 
-const {user} = require('./model/user');
+const { user } = require("./model/user");
 
- console.log(config.get('level'));
+console.log(config.get("level"));
 
 //  let newUser = new user({
 //     fullname : 'nader afshar',
@@ -14,33 +14,39 @@ const {user} = require('./model/user');
 //      password:'123'
 //  })
 
-
 const app = express();
 app.use(express.json());
 
-app.post('/api/users',(req,res)=>{
-   const body = _.pick(req.body,['fullname','email','password']);
-   console.log(req.body);
-   let newuser = new user(body);
-   newuser.save().then((usr)=>{
-        console.log('user added',usr);
-        res.status(200).json(usr);
-    },(err)=>{req.body
-        res.status(400).json({error:`somthings went wrong . ${err}`});
-    });   
+app.post("/api/users", async (req, res) => {
+  try {
+    const body = _.pick(req.body, ["fullname", "email", "password"]);
+    console.log(req.body);
+    let newuser = new user(body);
+
+    await newuser.save();
+    res.status(200).json(newuser);
+
+  } catch (e) {
+    res.status(400).json({ error: `somthings went wrong . ${e}` });
+  }
 });
 
-app.post('/api/login',(req,res)=>{
-    const body = _.pick(req.body,['email','password']);
-    user.findByCredentials(body.email,body.password).then((user)=>{
-        user.generateAuthToken().then((token)=>{
-                res.header('x-auth',token).status(200).send(token)
-        },(err)=>{
-            res.status(400).json({error:`something went wrong${err}`})
-        });
-    });
+app.post("/api/login", async(req, res) => {
+
+    try{
+        const body = _.pick(req.body, ["email", "password"]);
+        let usr = await user.findByCredentials(body.email, body.password)
+        let token = await  usr.generateAuthToken();
+        res.header("x-auth", token).status(200).send(token);
+    }
+    catch(e)
+    {
+        res.status(400).json({ error: `something went wrong${e}` });
+    }
+
+
 });
 
-app.listen(config.get('PORT'),()=>{
-        console.log(`server is running on port ${config.get('PORT')}`)
+app.listen(config.get("PORT"), () => {
+  console.log(`server is running on port ${config.get("PORT")}`);
 });
